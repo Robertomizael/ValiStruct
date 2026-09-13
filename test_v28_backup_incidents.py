c9c22d3e4abe7ec185ca1be7947e9f1961de2478
Robertomizael <robertojtr@gmail.com>
2026-09-13 10:00:00 -0700

    Remove duplicate root validation test

--- test_v28_backup_incidents.py
+++
@@ -1,4 +0,0 @@
-def test_v28_endpoints_exist():
-    import api
-    for name in ["admin_backup","admin_restore","list_incidents","create_incident","toggle_incident"]:
-        assert hasattr(api,name)
